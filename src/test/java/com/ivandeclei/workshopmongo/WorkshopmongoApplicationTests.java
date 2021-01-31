b6f357de7742f6cc032c639847df51f7dd813923
Ivandeclei <ivandeclei@gmail.com>
2021-01-31 11:33:40 -0300

    Criacao do projeto

---
+++ src/test/java/com/ivandeclei/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandeclei.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
